b744523bd9996920b598e42902d819a5acda355d
Nguyễn Công Viên <viennguyen@MacBook-Pro-Vien-Nguyen.local>
2025-09-17 14:23:49 +0700

    Upload full portfolio project

--- supabase/functions/hello-world/index.ts
+++ supabase/functions/hello-world/index.ts
@@ -7,17 +7,17 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 
 console.log("Hello from Functions!")
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
+// Deno.serve(async (req) => {
+//   const { name } = await req.json()
+//   const data = {
+//     message: `Hello ${name}!`,
+//   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+//   return new Response(
+//     JSON.stringify(data),
+//     { headers: { "Content-Type": "application/json" } },
+//   )
+// })
 
 /* To invoke locally:
 
