520692efcc394ab8842ed3ac1fd213ed99808253
Nguyễn Công Viên <viennguyen@192.168.2.187>
2025-09-17 23:49:45 +0700

    Update project

--- common/constants/carreers.ts
+++ common/constants/carreers.ts
@@ -39,21 +39,39 @@ export const CAREERS: CareerProps[] = [
     isShow: true,
   },
   {
-    position: "UI/UX Designer Intern",
+    position: "UI/UX Designer",
+    company: "SAPP Academy",
+    logo: "/images/careers/sapp academy.png",
+    location: "Le Thanh Nghi, Hai Ba Trung, Ha Noi, Viet Nam",
+    location_type: "Onsite",
+    type: "Full time",
+    start_date: "2024-04",
+    end_date: "2024-12",
+    industry: "Học viện đào tạo (Education)",
+    link: "https://sapp.edu.vn/",
+    responsibilities: [
+      "Developed a responsive and interactive landing page using Next.js to promote company services and solutions.",
+      "Ensured SEO optimization and fast performance by leveraging Next.js features such as static site generation.",
+      "Collaborated with design and marketing teams to align web presence with brand identity.",
+    ],
+    isShow: true,
+  },
+  {
+    position: "UI/UX Designer Inter",
     company: "NET5S",
     logo: "/images/careers/net5s.png",
-    location: "144 Trần Vỹ, Phú Diễn, Bắc Từ Liêm, Ha Noi, Viet Nam",
+    location: "144 Tran Vy, Phu Dien, Bac Tu Liem, Ha Noi, Viet Nam",
     location_type: "Onsite",
     type: "Full time",
     start_date: "2023-12",
     end_date: "2024-03",
-    industry: "Học viện đào tạo (Education)",
+    industry: "Công nghệ và Truyền thông",
     link: "https://net5s.vn/",
     responsibilities: [
       "Designed internal UI templates and layouts for web pages and landing pages to be used by developers.",
       "Created visually consistent designs following brand guidelines for marketing and product use.",
       "Collaborated with design and development teams internally to ensure templates are ready for implementation."
-    ]
+    ],
     isShow: true,
   },
 ];
